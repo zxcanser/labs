67a6c542802d300b8cfb271f3b90c3dc36b65706
zxcanser <90441639+zxcanser@users.noreply.github.com>
2022-01-13 11:06:54 +0500

    Add files via upload

--- lab4.c
+++ lab4.c
@@ -1,23 +1,35 @@
-#include <stdio.h>
 #include <math.h>
-
-int main() {
-  double x = 0.0,h,i,k = 0;
-  printf("Enter h -> ");
-  scanf("%lf", &h);
-  printf("x\t\tf(x)\n");
-  i = 2 / h;
-  for (k = 0; k <= i; k++) {
-    if (k <= i / 2) {
-      double f = 8*pow(x,3)*cos(x);
-      printf("%lf\t%lf\n", x, f);
-      x = x + h;
+#include <stdio.h>
+double f(double x)
+{
+  if (x <= 1 && x >= 0)
+      return 8*pow(x,3)*cos(x);
+  else
+      return log(1+sqrt(x))-cos(x);
+}
+double integral(double a, double b, int n)
+{
+    double h = (b - a) / n;
+    double S = 0;
+    for (int i = 0; i <= n - 1; i++)
+    {
+        S += f((a + i * h) + h / 2);
     }
-    if (k > i / 2) {
-      double f = log10(1+sqrt(x))-cos(x);
-      printf("%lf\t%lf\n", x, f);
-      x = x + h;
+    return S * h;
+}
+int main()
+{
+    int a = 0, b = 2, n = 10;
+    double eps, I, I1, d = 1;
+    printf("Enter h -> ");
+    scanf("%lf,\n", &eps);
+    while (d >= eps)
+    {
+        I1 = integral(a, b, n);
+        d = fabs(I1 - I) / 3;
+        I = I1;
+        n = n * 2;
     }
-  }
-return 0;
+    printf("I = %.5lf\n", I1);
+    return 0;
 }
